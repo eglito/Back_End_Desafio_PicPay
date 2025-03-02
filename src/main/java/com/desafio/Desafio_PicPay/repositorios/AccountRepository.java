3fa7aa912d13fac04a4f30bbb2e60e502cba7fe0
eglito <aleeglito@gmail.com>
2025-03-02 12:30:14 -0300

    uuid para localização da entidade via repositório

--- src/main/java/com/desafio/Desafio_PicPay/repositorios/AccountRepository.java
+++ src/main/java/com/desafio/Desafio_PicPay/repositorios/AccountRepository.java
@@ -4,6 +4,8 @@ import com.desafio.Desafio_PicPay.adapters.outputAdapters.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
 }
