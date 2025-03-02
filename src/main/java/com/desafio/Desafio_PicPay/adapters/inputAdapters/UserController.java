9b5439f58354f4134a17a1f95dc6de931ba8775d
eglito <aleeglito@gmail.com>
2025-03-02 12:26:26 -0300

    Introdução do parâmetro do tipo @PathVariable para o iD, modificações na URL dos endpoints

--- src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/UserController.java
+++ src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/UserController.java
@@ -1,5 +1,6 @@
 package com.desafio.Desafio_PicPay.adapters.inputAdapters;
 
+import com.desafio.Desafio_PicPay.domain.dtos.EmailResponseDTO;
 import com.desafio.Desafio_PicPay.domain.dtos.UserRequestDTO;
 import com.desafio.Desafio_PicPay.domain.dtos.UserResponseDTO;
 import com.desafio.Desafio_PicPay.services.UserServices;
@@ -7,38 +8,46 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
 
+@Controller
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    private final UserServices services;
+    private UserServices services;
 
-    @PostMapping(value = "/create_user")
+    @PostMapping(value = "/new_user")
     public ResponseEntity<Void> createUser(@RequestBody UserRequestDTO userRequestDTO){
-        services.creatUser(userRequestDTO);
+        services.createUser(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/user/{id}")
-    public ResponseEntity<UserResponseDTO> getUser(Long id){
-        UserResponseDTO user = services.getUser(id).getBody();
-        return ResponseEntity.ok(user);
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserResponseDTO> getUserForId(@PathVariable UUID id){
+        return ResponseEntity.ok(services.getUser(id).getBody());
+    }
+
+    @GetMapping("/emails")
+    public ResponseEntity<List<EmailResponseDTO>> getAllEmails(){
+        return ResponseEntity.ok(services.getAll());
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Void> updateUser(@RequestBody UserRequestDTO userRequestDTO, Long id){
+    public ResponseEntity<Void> updateUser(@RequestBody UserRequestDTO userRequestDTO,@PathVariable UUID id){
         services.updateUser(userRequestDTO, id);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteUser(Long id){
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id){
         ResponseEntity response = services.deleteUser(id);
         return ResponseEntity.ok().build();
     }
