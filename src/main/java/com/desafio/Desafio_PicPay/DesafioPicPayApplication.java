0f41a90d5f3376d86c8515ae2cdef7d2c3bc000d
eglito <aleeglito@gmail.com>
2025-03-02 12:32:02 -0300

    Introdução de anotações relativas a busca e inicialização do repositório

--- src/main/java/com/desafio/Desafio_PicPay/DesafioPicPayApplication.java
+++ src/main/java/com/desafio/Desafio_PicPay/DesafioPicPayApplication.java
@@ -3,12 +3,11 @@ package com.desafio.Desafio_PicPay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-		"com.desafio.Desafio_PicPay.domain.entity",
-		"com.desafio.Desafio_PicPay.adapters.outputAdapters"
-})
+//@EnableJpaRepositories(basePackages = "com.desafio.Desafio_PicPay.repositorios.UserRepository")
+//@EntityScan("com.desafio.Desafio_PicPay.adapters.outputAdapters")
 public class DesafioPicPayApplication {
 
 	public static void main(String[] args) {
