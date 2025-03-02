c9323fd74a286b86c6e369c2196d7b6119dea0d4
eglito <aleeglito@gmail.com>
2025-03-02 12:18:36 -0300

    Configuração da Classe COntroller para os endpoints responsáveis pela entidade Account

--- src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/AccountController.java
+++ src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/AccountController.java
@@ -1,4 +1,33 @@
 package com.desafio.Desafio_PicPay.adapters.inputAdapters;
 
+import com.desafio.Desafio_PicPay.domain.dtos.AccountDTO;
+import com.desafio.Desafio_PicPay.domain.dtos.EmailResponseDTO;
+import com.desafio.Desafio_PicPay.services.AccountServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private AccountServices services;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDTO> getAccount(@PathVariable UUID id){
+        return ResponseEntity.ok(services.returnAccount(id).getBody());
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity updateAccount(@RequestBody AccountDTO accountDTO, @PathVariable UUID id){
+        return ResponseEntity.ok(services.putAccount(id, accountDTO).getBody());
+    }
+
 }
