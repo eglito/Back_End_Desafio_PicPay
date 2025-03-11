60c0825203c422caaf19e0e5b6b8a6a7c83f8269
eglito <aleeglito@gmail.com>
2025-03-11 14:24:13 -0300

    Regras de negócio para transferências

--- src/main/java/com/desafio/Desafio_PicPay/services/TransferServices.java
+++ src/main/java/com/desafio/Desafio_PicPay/services/TransferServices.java
@@ -1,8 +1,9 @@
 package com.desafio.Desafio_PicPay.services;
 
+import com.desafio.Desafio_PicPay.adapters.outputAdapters.AccountEntity;
 import com.desafio.Desafio_PicPay.adapters.outputAdapters.UserEntity;
 import com.desafio.Desafio_PicPay.domain.TypeAccount;
-import com.desafio.Desafio_PicPay.domain.dtos.AccountDTO;
+import com.desafio.Desafio_PicPay.domain.dtos.TransferDTO;
 import com.desafio.Desafio_PicPay.repositorios.AccountRepository;
 import com.desafio.Desafio_PicPay.repositorios.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +16,47 @@ import java.util.UUID;
 public class TransferServices {
 
     @Autowired
-    private final UserRepository userRepository;
     private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
-    public TransferServices(UserRepository userRepository, AccountRepository accountRepository){
-        this.userRepository = userRepository;
+    public TransferServices(AccountRepository accountRepository, UserRepository userRepository){
         this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
     }
 
+    //Verificação do status de contas
     public boolean checkTransferForStatus(UUID iDTransmitter, UUID iDReceiver){
 
         UserEntity userEntityTransmitter = userRepository.findById(iDTransmitter).get();
         UserEntity userEntityReceiver = userRepository.findById(iDReceiver).get();
 
-        if(!userEntityTransmitter.getStatus().equals(TypeAccount.CORPORATE) &&
+        if(userEntityTransmitter.getStatus().equals(TypeAccount.CORPORATE) &&
             userEntityReceiver.getStatus().equals(TypeAccount.PERSONAL)){
             return false;
         }
         return true;
     }
 
-    public ResponseEntity<Void> trasferAccount (UUID iDTransmitter, AccountDTO dtoTransmitter, UUID iDReceiver, AccountDTO dtoReceiver){
+    public ResponseEntity<Void> trasferAccount (TransferDTO transferDTO){
 
-        if(checkTransferForStatus(iDTransmitter,iDReceiver)){
-            throw new IllegalArgumentException("CORPORATE não podem tranferir para PERSONAL");
+        if(!checkTransferForStatus(transferDTO.payer(), transferDTO.payee())){
+            throw new IllegalArgumentException("CORPORATE não podem transferir para PERSONAL");
         }
 
+        UserEntity userEntityTransmitter = userRepository.findById(transferDTO.payer()).get();
+        AccountEntity accountTransmitter = userEntityTransmitter.getAccount();
+        UserEntity userEntityReceiver = userRepository.findById(transferDTO.payee()).get();
+        AccountEntity accountReceiver = userEntityReceiver.getAccount();
+
+        if(accountTransmitter.getBalance() >= transferDTO.value()){
+            accountTransmitter.setBalance(accountTransmitter.getBalance() - transferDTO.value());
+            accountReceiver.setBalance(accountReceiver.getBalance() + transferDTO.value());
+        } else {
+            throw new IllegalArgumentException("Saldo de transmitter é insuficiente");
+        }
+
+        accountRepository.save(accountTransmitter);
+        accountRepository.save(accountReceiver);
 
         return ResponseEntity.ok().build();
     }
