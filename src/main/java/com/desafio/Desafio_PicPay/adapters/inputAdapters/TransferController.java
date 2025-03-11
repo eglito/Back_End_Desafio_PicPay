bb2457a134aa28de4061e289360bd86e602a34fb
eglito <aleeglito@gmail.com>
2025-03-11 14:23:38 -0300

    Endpoint para Transfer

--- src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/TransferController.java
+++ src/main/java/com/desafio/Desafio_PicPay/adapters/inputAdapters/TransferController.java
@@ -1,5 +1,28 @@
 package com.desafio.Desafio_PicPay.adapters.inputAdapters;
 
+import com.desafio.Desafio_PicPay.domain.dtos.TransferDTO;
+import com.desafio.Desafio_PicPay.services.TransferServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/transfer")
 public class TransferController {
 
+    @Autowired
+    private TransferServices services;
+
+    @PutMapping
+    public ResponseEntity transfer(@RequestBody TransferDTO transferDTO){
+        return services.trasferAccount(transferDTO);
+    }
 }
