31b69a8c3c5f491c02b94a0c6cb0e30cf9af6aa1
eglito <aleeglito@gmail.com>
2025-03-11 09:06:46 -0300

    Compo de enum, TypeAccount, no DTO

--- src/main/java/com/desafio/Desafio_PicPay/domain/dtos/UserRequestDTO.java
+++ src/main/java/com/desafio/Desafio_PicPay/domain/dtos/UserRequestDTO.java
@@ -1,8 +1,11 @@
 package com.desafio.Desafio_PicPay.domain.dtos;
 
+import com.desafio.Desafio_PicPay.domain.TypeAccount;
+
 public record UserRequestDTO(
         String name,
         String cpf,
         String email,
+        TypeAccount status,
         String password) {
 }
