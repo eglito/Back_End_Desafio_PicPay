620617c90d50665f6553a63f1c66c970ba287dc4
eglito <aleeglito@gmail.com>
2025-03-29 21:02:19 -0300

    Criação da classe de teste unitário para a persistência dos Users. Teste é realizado pela classe findFirstUserCase2.

---
+++ src/test/java/com/desafio/Desafio_PicPay/repositorios/UserRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.desafio.Desafio_PicPay.repositorios;
+
+import com.desafio.Desafio_PicPay.adapters.outputAdapters.UserEntity;
+import com.desafio.Desafio_PicPay.domain.TypeAccount;
+import com.desafio.Desafio_PicPay.domain.dtos.UserRequestDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static com.desafio.Desafio_PicPay.domain.TypeAccount.PERSONAL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get user successfully from DB")
+    void findFirstUserCase1(){
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO("Alexandre","410.569.444-40", "alexandre@gmail.com", PERSONAL, "@Araraquara16");
+        UserEntity userEntity = this.createUser(userRequestDTO);
+
+        Optional<UserEntity> result = this.userRepository.findById(userEntity.getId());
+
+        assertThat(result.isPresent()).isTrue();
+
+        assertThat(userEntity.getId()).isNotNull();
+
+        assertThat(userRequestDTO.name()).isEqualTo(userEntity.getName());
+    }
+
+
+    private UserEntity createUser(UserRequestDTO userRequestDTO){
+
+        UserEntity newUser = new UserEntity(userRequestDTO);
+        this.entityManager.persist(newUser);
+        return newUser;
+    }
+
+}
